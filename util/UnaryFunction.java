dca0024dab9e0a03c971825fcd56a3f6272464ff
Joseph Dale <jdale@dnanexus.com>
2013-08-04 20:33:20 -0700

    Move files from CVS.

---
+++ util/UnaryFunction.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * Interface representing a function of one argument which returns a
+ * value of any type.
+ *
+ * @author <a href="mailto:jmdale@gmail.com">Joseph Dale</a>
+ * @version 20130804
+ */
+public interface UnaryFunction {
+
+    public Object apply(Object o);
+
+}
---
+++ util/UnaryPredicate.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * Interface representing a function of one argument which returns a
+ * boolean.
+ *
+ * @author <a href="mailto:jmdale@gmail.com">Joseph Dale</a>
+ * @version 20130804
+ */
+public interface UnaryPredicate {
+
+    public boolean apply(Object o);
+
+}
