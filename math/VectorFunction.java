90af808088821aba88e56aa63f3693221f6ca6d4
Joseph Dale <jdale@dnanexus.com>
2013-08-05 23:18:09 -0700

    Move from CVS.

---
+++ math/VectorFunction.java
@@ -0,0 +1,13 @@
+package math;
+
+/**
+ * A function from DoubleVectors to doubles.
+ *
+ * @author <a href="mailto:jmdale@gmail.com">Joseph Dale</a>
+ * @version 20070817
+ */
+public abstract class VectorFunction {
+
+    public abstract double apply(DoubleVector x);
+
+}
---
+++ math/VectorFunctional.java
@@ -0,0 +1,13 @@
+package math;
+
+/**
+ * A function from DoubleVectors to DoubleVectors.
+ *
+ * @author <a href="mailto:jmdale@gmail.com">Joseph Dale</a>
+ * @version 20071021
+ */
+public abstract class VectorFunctional {
+
+    public abstract DoubleVector apply(DoubleVector x);
+
+}
